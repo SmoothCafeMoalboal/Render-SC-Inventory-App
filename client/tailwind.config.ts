e340303328f4636df4027227f6e71365ab01a73f
SmoothCafeMoalboal <smoothcafemoalboal@gmail.com>
2025-08-09 12:27:40 +0800

    fix(client): add custom border color for Tailwind

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,7 +6,12 @@ const config: Config = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: 'hsl(201.4286, 30.4348%, 90.9804%)',
+        // add other custom colors here if needed
+      },
+    },
   },
   plugins: [],
 };
